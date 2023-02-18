47875f4a15670bebfa7313de681dd31939c94c56
Juan <juanfelipeoficial99@gmail.com>
2023-02-18 18:58:30 -0300

    Small improvements to performance

--- scripts.tsx
+++ scripts.tsx
@@ -1,8 +1,8 @@
 var telas;
-window.onload = ( ()=>{
-    var containers = document.querySelectorAll('#container').forEach(container =>{
-        container.style.width="0";
-        container.style.opacity="1";
+window.onload = (() => {
+    var title_containers = document.querySelectorAll('#title-container').forEach(title_container => {
+        title_container.style.width = "0";
+        title_container.style.opacity = "1";
     });
 })
 window.addEventListener("scroll", function () {
@@ -14,12 +14,12 @@ window.addEventListener("scroll", function () {
     let proj = document.getElementById('projetos');
     let cont = document.getElementById('contato');
 
-    var cliHei =document.documentElement.clientHeight;
-    var offSet= cliHei*0.30;
+    var cliHei = document.documentElement.clientHeight;
+    var offSet = cliHei * 0.30;
     var scr = document.documentElement?.scrollTop;
     telas = [teini, sm, con, proj, cont];
 
-    console.log("CH",document.documentElement.clientHeight);
+    console.log("CH", document.documentElement.clientHeight);
     console.log("scr:", scr);
     if (teini?.clientHeight !== undefined) {
         if (scr >= (teini?.getClientRects().item(0)?.top + scr) && scr <= (teini?.getClientRects().item(0)?.bottom + scr)) {
@@ -29,29 +29,29 @@ window.addEventListener("scroll", function () {
     }
 
     if (sm?.clientHeight !== undefined) {
-        if (scr >= (sm?.getClientRects().item(0)?.top + scr -offSet) && scr <= (sm?.getClientRects().item(0)?.bottom + scr)) {
+        if (scr >= (sm?.getClientRects().item(0)?.top + scr - offSet) && scr <= (sm?.getClientRects().item(0)?.bottom + scr)) {
             changeColors(sm?.id);
 
         }
     }
     if (con?.clientHeight !== undefined) {
-        if (scr >= (con?.getClientRects().item(0)?.top + scr -offSet) && scr <= (con?.getClientRects().item(0)?.bottom + scr)) {
+        if (scr >= (con?.getClientRects().item(0)?.top + scr - offSet) && scr <= (con?.getClientRects().item(0)?.bottom + scr)) {
             changeColors(con?.id);
 
         }
     }
 
     if (proj?.clientHeight !== undefined) {
-        if (scr >= (proj?.getClientRects().item(0)?.top + scr -offSet) && scr <= (proj?.getClientRects().item(0)?.bottom + scr)) {
+        if (scr >= (proj?.getClientRects().item(0)?.top + scr - offSet) && scr <= (proj?.getClientRects().item(0)?.bottom + scr)) {
             changeColors(proj?.id);
 
         }
     }
 
-    console.log("cont Height:", cont?.getClientRects().item(0)?.top);
-    console.log("cont Bottom:", cont?.getClientRects().item(0)?.bottom);
+    console.log("cont Height:", cont?.getClientRects().item(0)?.top + scr - offSet);
+    console.log("cont Bottom:", cont?.getClientRects().item(0)?.bottom + scr - offSet);
     if (cont?.clientHeight !== undefined) {
-        if (scr >= (cont?.getClientRects().item(0)?.top + scr -offSet) && scr <= (cont?.getClientRects().item(0)?.bottom + scr)) {
+        if (scr >= (cont?.getClientRects().item(0)?.top + scr - offSet) && scr <= (cont?.getClientRects().item(0)?.bottom + scr)) {
             changeColors(cont?.id);
 
         }
@@ -61,7 +61,7 @@ window.addEventListener("scroll", function () {
 );
 
 function changeColors(nomeTela) {
-
+    console.log("NOME TELA:", nomeTela);
     for (let i = 0; i < telas.length; i++) {
         if (telas[i]?.id === nomeTela) {
             var grad = telas[i]?.children[0];
@@ -70,17 +70,25 @@ function changeColors(nomeTela) {
                 titulo.style.width = "50%";
                 titulo.style.transition = "width 1s ease";
             }
+                console.log(nomeTela," ENTROU IF");
 
+            grad.style.display = "";
             grad.style.opacity = "1";
             grad.style.transition = "opacity 1s linear";
 
         }
         else {
+            console.log(nomeTela," ENTROU ELSE");
             var grad = telas[i]?.children[0];
             if (telas[i]?.id !== 'tela-inicial') {
                 var titulo = telas[i]?.children[1];
                 titulo.style.width = "0";
                 titulo.style.transition = "width 1s ease";
+            }
+            if(telas[i]?.id === nomeTela){//caso primeiro if (telas[i]?.id === nomeTela) { falhe
+                setTimeout(() => {
+                        grad.style.display = "none";
+                    }, 1000);
             }
             grad.style.opacity = "0";
             grad.style.transition = "opacity 1s linear";
